e12007f5e974a4a64dc1ff407d0be95c7a27f62a
chrismitsdev <chrismits88@gmail.com>
2024-09-30 01:32:16 +0300

    updated deps

--- src/components/page/accomodation/faq.tsx
+++ src/components/page/accomodation/faq.tsx
@@ -21,7 +21,9 @@ function Faq() {
             <AccordionTrigger>{'Accordion Trigger 1'}</AccordionTrigger>
             <AccordionContent>
               <div className='px-4 py-3'>
-                {'This is the content for accordion trigger 1'}
+                {
+                  'Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi officia autem fugit explicabo temporibus molestiae veniam iure cupiditate nulla, laboriosam maiores libero, beatae quaerat. Nihil veniam architecto molestias nulla fugit totam! Inventore sit sed, eligendi molestias et consectetur reiciendis, fuga laudantium aspernatur eius nulla dolorem vel obcaecati! Velit, sint nisi.'
+                }
               </div>
             </AccordionContent>
           </AccordionItem>
@@ -29,7 +31,9 @@ function Faq() {
             <AccordionTrigger>{'Accordion Trigger 2'}</AccordionTrigger>
             <AccordionContent>
               <div className='px-4 py-3'>
-                {'This is the content for accordion trigger 2'}
+                {
+                  'Lorem ipsum dolor sit amet consectetur adipisicing elit. In nam ab laborum error ea rem nihil sint, nisi voluptates nesciunt deleniti exercitationem non corporis cum modi ipsam. Quis enim consequuntur repellat blanditiis tenetur delectus eveniet porro placeat a expedita aliquam quod maxime quisquam repudiandae nihil deleniti, ratione cupiditate dolorem perspiciatis.'
+                }
               </div>
             </AccordionContent>
           </AccordionItem>
@@ -37,7 +41,9 @@ function Faq() {
             <AccordionTrigger>{'Accordion Trigger 3'}</AccordionTrigger>
             <AccordionContent>
               <div className='px-4 py-3'>
-                {'This is the content for accordion trigger 3'}
+                {
+                  'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Natus amet ipsum omnis expedita distinctio, odit soluta sunt sed, labore incidunt optio consequatur quibusdam, exercitationem libero enim! Unde id consectetur placeat molestiae voluptate. Accusantium esse ab vel magni distinctio quasi, a, culpa ipsam amet itaque, quaerat ratione. Consequatur veritatis rem debitis?'
+                }
               </div>
             </AccordionContent>
           </AccordionItem>
