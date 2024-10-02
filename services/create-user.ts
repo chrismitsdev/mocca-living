b6acdd2a6d2feb6956ab924638324ed33b3b3d48
chrismitsdev <chrismits88@gmail.com>
2024-10-02 14:48:54 +0300

    create-user refactored

--- services/create-user.ts
+++ services/create-user.ts
@@ -2,13 +2,20 @@ import postgres from 'postgres'
 
 type User = {
   id: number
+  created_at: string
   name: string
   email: string
 }
 
-const sql = postgres(process.env.DATABASE_URL as string, {ssl: true})
-
 export async function createUser(name: string, email: string) {
+  const databaseUrl = process.env.DATABASE_URL
+
+  if (!databaseUrl) {
+    throw new Error('DATABASE_URL is not set')
+  }
+
+  const sql = postgres(databaseUrl, {ssl: true})
+
   try {
     const response = await sql<User[]>`
       INSERT INTO users (name, email)
@@ -17,8 +24,9 @@ export async function createUser(name: string, email: string) {
     `
 
     if (response.length > 0) {
-      const {name, email} = response[0]
-      console.log('User successfully created!', {name, email})
+      console.log(`✅ User ${response[0].name} successfully created!`)
+    } else {
+      console.log('❌ There was an error in creating the user')
     }
   } catch (error) {
     if (error instanceof Error) {
@@ -26,47 +34,8 @@ export async function createUser(name: string, email: string) {
     } else {
       console.error(error)
     }
-
     throw error
+  } finally {
+    await sql.end()
   }
 }
-
-// import postgres from 'postgres'
-
-// type User = {
-//   id: number
-//   name: string
-//   email: string
-// }
-
-// export async function createUser(name: string, email: string) {
-//   const databaseUrl = process.env.DATABASE_URL
-
-//   if (!databaseUrl) {
-//     throw new Error('DATABASE_URL is not set')
-//   }
-
-//   const sql = postgres(databaseUrl, {ssl: true})
-
-//   try {
-//     const response = await sql<User[]>`
-//       INSERT INTO users (name, email)
-//       VALUES (${name}, ${email})
-//       RETURNING *
-//     `
-
-//     if (response.length > 0) {
-//       const {name, email} = response[0]
-//       console.log('User successfully created!', {name, email})
-//     }
-//   } catch (error) {
-//     if (error instanceof Error) {
-//       console.error(error.message)
-//     } else {
-//       console.error(error)
-//     }
-//     throw error
-//   } finally {
-//     await sql.end()
-//   }
-// }
