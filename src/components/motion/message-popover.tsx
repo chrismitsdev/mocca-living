05e0f042aa2927c2c048446008934e9593a07982
chrismitsdev <chrismits88@gmail.com>
2024-10-09 20:33:35 +0300

    message popover fix attempt 2

--- src/components/motion/message-popover.tsx
+++ src/components/motion/message-popover.tsx
@@ -33,16 +33,15 @@ function MessagePopover() {
   })
 
   React.useEffect(function () {
-    function handleClosePopover(event: KeyboardEvent) {
+    function handleEscapePress(event: KeyboardEvent) {
       if (event.key === 'Escape') {
         closeMenu()
       }
     }
 
-    document.addEventListener('keydown', handleClosePopover)
-
+    document.addEventListener('keydown', handleEscapePress)
     return function () {
-      document.removeEventListener('keydown', handleClosePopover)
+      document.removeEventListener('keydown', handleEscapePress)
     }
   }, [])
 
@@ -56,8 +55,7 @@ function MessagePopover() {
           style={{
             borderRadius: 4
           }}
-          // onClick={openMenu}
-          onMouseUp={openMenu}
+          onClick={openMenu}
         >
           <MessageCircleMoreIcon size={24} />
         </motion.button>
@@ -66,46 +64,48 @@ function MessagePopover() {
           {isOpen && (
             <motion.div
               layoutId={`popover-${uniqueId}`}
-              className='p-4 overflow-hidden bg-surface-1 border outline-none'
+              className='p-4 space-y-4 bg-surface-1 border outline-none'
               style={{
                 borderRadius: 4
               }}
               ref={formContainerRef}
             >
-              <div className='space-y-4'>
-                <a
-                  href='viber://chat?number=+306936998859'
-                  className='flex items-center gap-2'
-                  aria-label='Viber messaging'
-                >
+              <a
+                href='viber://chat?number=+306936998859'
+                className='flex items-center gap-2'
+                aria-label='Viber messaging'
+              >
+                <span>
                   <LogoViber />
-                  <Typography variant='large'>{'Viber'}</Typography>
-                </a>
-                <a
-                  href='whatsapp://send?phone=+306936998859'
-                  className='flex items-center gap-2'
-                  aria-label='WhatsApp messaging'
-                >
+                </span>
+                <Typography variant='large'>{'Viber'}</Typography>
+              </a>
+              <a
+                href='whatsapp://send?phone=+306936998859'
+                className='flex items-center gap-2'
+                aria-label='WhatsApp messaging'
+              >
+                <span>
                   <LogoWhatsApp />
-                  <Typography variant='large'>{'WhatsApp'}</Typography>
-                </a>
-                <a
-                  className='flex items-center gap-2'
-                  href='sms:+306936998859'
-                  aria-label='Open messaging app to send a text message'
+                </span>
+                <Typography variant='large'>{'WhatsApp'}</Typography>
+              </a>
+              <a
+                className='flex items-center gap-2'
+                href='sms:+306936998859'
+                aria-label='Open messaging app to send a text message'
+              >
+                <Button
+                  className='rounded-md'
+                  size='icon-small'
+                  asChild
                 >
-                  <Button
-                    className='rounded-md'
-                    size='icon-small'
-                    asChild
-                  >
-                    <span>
-                      <MessageCircleIcon size={24} />
-                    </span>
-                  </Button>
-                  <Typography variant='large'>{'SMS'}</Typography>
-                </a>
-              </div>
+                  <span>
+                    <MessageCircleIcon size={24} />
+                  </span>
+                </Button>
+                <Typography variant='large'>{'SMS'}</Typography>
+              </a>
             </motion.div>
           )}
         </AnimatePresence>
@@ -117,119 +117,3 @@ function MessagePopover() {
 MessagePopover.displayName = 'MessagePopover'
 
 export {MessagePopover}
-
-// export default function Popover() {
-//   const uniqueId = useId();
-//   const formContainerRef = useRef<HTMLDivElement>(null);
-//   const [isOpen, setIsOpen] = useState(false);
-//   const [note, setNote] = useState<null | string>(null);
-
-//   const openMenu = () => {
-//     setIsOpen(true);
-//   };
-
-//   const closeMenu = () => {
-//     setIsOpen(false);
-//     setNote(null);
-//   };
-
-//   useClickOutside(formContainerRef, () => {
-//     closeMenu();
-//   });
-
-//   useEffect(() => {
-//     const handleKeyDown = (event: KeyboardEvent) => {
-//       if (event.key === 'Escape') {
-//         closeMenu();
-//       }
-//     };
-
-//     document.addEventListener('keydown', handleKeyDown);
-
-//     return () => {
-//       document.removeEventListener('keydown', handleKeyDown);
-//     };
-//   }, []);
-
-//   return (
-//     <MotionConfig transition={TRANSITION}>
-//       <div className='relative flex items-center justify-center'>
-//         <motion.button
-//           key='button'
-//           layoutId={`popover-${uniqueId}`}
-//           className='flex h-9 items-center border border-zinc-950/10 bg-white px-3 text-zinc-950 dark:border-zinc-50/10 dark:bg-zinc-700 dark:text-zinc-50'
-//           style={{
-//             borderRadius: 8,
-//           }}
-//           onClick={openMenu}
-//         >
-//           <motion.span
-//             layoutId={`popover-label-${uniqueId}`}
-//             className='text-sm'
-//           >
-//             Add Note
-//           </motion.span>
-//         </motion.button>
-
-//         <AnimatePresence>
-//           {isOpen && (
-//             <motion.div
-//               ref={formContainerRef}
-//               layoutId={`popover-${uniqueId}`}
-//               className='absolute h-[200px] w-[364px] overflow-hidden border border-zinc-950/10 bg-white outline-none dark:bg-zinc-700'
-//               style={{
-//                 borderRadius: 12,
-//               }}
-//             >
-//               <form
-//                 className='flex h-full flex-col'
-//                 onSubmit={(e) => {
-//                   e.preventDefault();
-//                 }}
-//               >
-//                 <motion.span
-//                   layoutId={`popover-label-${uniqueId}`}
-//                   aria-hidden='true'
-//                   style={{
-//                     opacity: note ? 0 : 1,
-//                   }}
-//                   className='absolute left-4 top-3 select-none text-sm text-zinc-500 dark:text-zinc-400'
-//                 >
-//                   Add Note
-//                 </motion.span>
-//                 <textarea
-//                   className='h-full w-full resize-none rounded-md bg-transparent px-4 py-3 text-sm outline-none'
-//                   autoFocus
-//                   onChange={(e) => setNote(e.target.value)}
-//                 />
-//                 <div key='close' className='flex justify-between px-4 py-3'>
-//                   <button
-//                     type='button'
-//                     className='flex items-center'
-//                     onClick={closeMenu}
-//                     aria-label='Close popover'
-//                   >
-//                     <ArrowLeftIcon
-//                       size={16}
-//                       className='text-zinc-900 dark:text-zinc-100'
-//                     />
-//                   </button>
-//                   <button
-//                     className='relative ml-1 flex h-8 shrink-0 scale-100 select-none appearance-none items-center justify-center rounded-lg border border-zinc-950/10 bg-transparent px-2 text-sm text-zinc-500 transition-colors hover:bg-zinc-100 hover:text-zinc-800 focus-visible:ring-2 active:scale-[0.98] dark:border-zinc-50/10 dark:text-zinc-50 dark:hover:bg-zinc-800'
-//                     type='submit'
-//                     aria-label='Submit note'
-//                     onClick={() => {
-//                       closeMenu();
-//                     }}
-//                   >
-//                     Submit Note
-//                   </button>
-//                 </div>
-//               </form>
-//             </motion.div>
-//           )}
-//         </AnimatePresence>
-//       </div>
-//     </MotionConfig>
-//   );
-// }
