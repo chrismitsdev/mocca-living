d02efd6c6bfabe7cdd6212011649c79511c356e8
chrismitsdev <chrismits88@gmail.com>
2024-10-11 14:48:59 +0300

    added motion fade up component

---
+++ src/components/motion/fade-up.tsx
@@ -0,0 +1,40 @@
+'use client'
+
+import {motion} from 'framer-motion'
+
+type FadeUpProps = {
+  delay?: number
+  duration?: number
+  once?: boolean
+  children: React.ReactNode
+}
+
+function FadeUp({
+  delay = 0,
+  duration = 0.5,
+  once = true,
+  children
+}: FadeUpProps) {
+  return (
+    <motion.div
+      variants={{
+        hidden: {opacity: 0, y: 16},
+        visible: {opacity: 1, y: 0}
+      }}
+      initial='hidden'
+      whileInView='visible'
+      transition={{
+        delay,
+        duration,
+        type: 'spring'
+      }}
+      viewport={{once}}
+    >
+      {children}
+    </motion.div>
+  )
+}
+
+FadeUp.displayName = 'FadeUp'
+
+export {FadeUp}
