ccb97cb5a90f300506dca0f5f034d9bc3c11e41e
chrismitsdev <chrismits88@gmail.com>
2025-03-15 16:21:17 +0200

    minor change in useScrollLock hook

--- src/hooks/useScrollLock.ts
+++ src/hooks/useScrollLock.ts
@@ -18,6 +18,7 @@ type UseScrollLockReturn = {
 type OriginalStyle = {
   overflow: CSSStyleDeclaration['overflow']
   paddingRight: CSSStyleDeclaration['paddingRight']
+  height: CSSStyleDeclaration['height']
 }
 
 const IS_SERVER = typeof window === 'undefined'
@@ -32,10 +33,10 @@ export function useScrollLock(
 
   const lock = () => {
     if (target.current) {
-      const {overflow, paddingRight} = target.current.style
+      const {overflow, paddingRight, height} = target.current.style
 
       // Save the original styles
-      originalStyle.current = {overflow, paddingRight}
+      originalStyle.current = {overflow, paddingRight, height}
 
       // Prevent width reflow
       if (widthReflow) {
@@ -55,8 +56,9 @@ export function useScrollLock(
         }px`
       }
 
-      // Lock the scroll
+      // Lock the scroll and apply full height
       target.current.style.overflow = 'hidden'
+      target.current.style.height = '100%'
 
       setIsLocked(true)
     }
@@ -70,6 +72,8 @@ export function useScrollLock(
       if (widthReflow) {
         target.current.style.paddingRight = originalStyle.current.paddingRight
       }
+
+      target.current.style.height = originalStyle.current.height
     }
 
     setIsLocked(false)
@@ -101,3 +105,107 @@ export function useScrollLock(
 
   return {isLocked, lock, unlock}
 }
+
+// import * as React from 'react'
+
+// const useIsomorphicLayoutEffect =
+//   typeof window !== 'undefined' ? React.useLayoutEffect : React.useEffect
+
+// type UseScrollLockOptions = {
+//   autoLock?: boolean
+//   lockTarget?: HTMLElement | string
+//   widthReflow?: boolean
+// }
+
+// type UseScrollLockReturn = {
+//   isLocked: boolean
+//   lock: () => void
+//   unlock: () => void
+// }
+
+// type OriginalStyle = {
+//   overflow: CSSStyleDeclaration['overflow']
+//   paddingRight: CSSStyleDeclaration['paddingRight']
+// }
+
+// const IS_SERVER = typeof window === 'undefined'
+
+// export function useScrollLock(
+//   options: UseScrollLockOptions = {}
+// ): UseScrollLockReturn {
+//   const {autoLock = true, lockTarget, widthReflow = true} = options
+//   const [isLocked, setIsLocked] = React.useState(false)
+//   const target = React.useRef<HTMLElement | null>(null)
+//   const originalStyle = React.useRef<OriginalStyle | null>(null)
+
+//   const lock = () => {
+//     if (target.current) {
+//       const {overflow, paddingRight} = target.current.style
+
+//       // Save the original styles
+//       originalStyle.current = {overflow, paddingRight}
+
+//       // Prevent width reflow
+//       if (widthReflow) {
+//         // Use window inner width if body is the target as global scrollbar isn't part of the document
+//         const offsetWidth =
+//           target.current === document.body
+//             ? window.innerWidth
+//             : target.current.offsetWidth
+//         // Get current computed padding right in pixels
+//         const currentPaddingRight =
+//           parseInt(window.getComputedStyle(target.current).paddingRight, 10) ||
+//           0
+
+//         const scrollbarWidth = offsetWidth - target.current.scrollWidth
+//         target.current.style.paddingRight = `${
+//           scrollbarWidth + currentPaddingRight
+//         }px`
+//       }
+
+//       // Lock the scroll
+//       target.current.style.overflow = 'hidden'
+
+//       setIsLocked(true)
+//     }
+//   }
+
+//   const unlock = () => {
+//     if (target.current && originalStyle.current) {
+//       target.current.style.overflow = originalStyle.current.overflow
+
+//       // Only reset padding right if we changed it
+//       if (widthReflow) {
+//         target.current.style.paddingRight = originalStyle.current.paddingRight
+//       }
+//     }
+
+//     setIsLocked(false)
+//   }
+
+//   useIsomorphicLayoutEffect(() => {
+//     if (IS_SERVER) return
+
+//     if (lockTarget) {
+//       target.current =
+//         typeof lockTarget === 'string'
+//           ? document.querySelector(lockTarget)
+//           : lockTarget
+//     }
+
+//     if (!target.current) {
+//       target.current = document.body
+//     }
+
+//     if (autoLock) {
+//       lock()
+//     }
+
+//     return () => {
+//       unlock()
+//     }
+//     // eslint-disable-next-line react-hooks/exhaustive-deps
+//   }, [autoLock, lockTarget, widthReflow])
+
+//   return {isLocked, lock, unlock}
+// }
