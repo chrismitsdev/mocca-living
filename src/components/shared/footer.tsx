71d4292e93928777f6e0babc7ca28dd5725b5945
chrismitsdev <chrismits88@gmail.com>
2025-03-17 13:56:01 +0200

    correction in adults accomodation translations

--- src/components/shared/footer.tsx
+++ src/components/shared/footer.tsx
@@ -187,7 +187,7 @@ const Footer: React.FC = () => {
                 </a>
               </Button>
             </div>
-            <LocaleSwitcher />
+            <LocaleSwitcher scrollTop />
           </FooterColumn>
         </div>
 
