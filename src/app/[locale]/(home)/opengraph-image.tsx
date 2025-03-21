5cee5d4cd1db17905a7a04b5e17a1444a173cc5f
chrismitsdev <chrismits88@gmail.com>
2025-03-22 00:33:18 +0200

    opengraph image with helper function

--- src/app/[locale]/(home)/opengraph-image.tsx
+++ src/app/[locale]/(home)/opengraph-image.tsx
@@ -1,18 +1,11 @@
 import {ImageResponse} from 'next/og'
 import {getTranslations} from 'next-intl/server'
-import {join} from 'node:path'
-import {readFile} from 'node:fs/promises'
+import {getOpengraphData} from '@/src/lib/getOpengraphData'
 
 export default async function Image({params}: Params) {
   const {locale} = await params
   const t = await getTranslations({locale, namespace: 'Metadata.Pages'})
-  const commisionerSemiBold = await readFile(
-    join(process.cwd(), 'assets/Commissioner-SemiBold.ttf')
-  )
-  const logoData = await readFile(
-    join(process.cwd(), 'public/images/other/opengraph.png')
-  )
-  const logoSrc = `data:image/png;base64,${logoData.toString('base64')}`
+  const {src, font} = await getOpengraphData()
 
   return new ImageResponse(
     (
@@ -30,7 +23,7 @@ export default async function Image({params}: Params) {
         }}
       >
         <img
-          src={logoSrc}
+          src={src}
           width='250'
         />
         <p
@@ -64,11 +57,84 @@ export default async function Image({params}: Params) {
       fonts: [
         {
           name: 'Commissioner',
-          data: commisionerSemiBold,
+          data: font,
           style: 'normal'
-          // weight: 700
         }
       ]
     }
   )
 }
+
+// import {ImageResponse} from 'next/og'
+// import {getTranslations} from 'next-intl/server'
+// import {join} from 'node:path'
+// import {readFile} from 'node:fs/promises'
+
+// export default async function Image({params}: Params) {
+//   const {locale} = await params
+//   const t = await getTranslations({locale, namespace: 'Metadata.Pages'})
+//   const commisionerSemiBold = await readFile(
+//     join(process.cwd(), 'assets/Commissioner-SemiBold.ttf')
+//   )
+//   const logoData = await readFile(
+//     join(process.cwd(), 'public/images/other/opengraph.png')
+//   )
+//   const logoSrc = `data:image/png;base64,${logoData.toString('base64')}`
+
+//   return new ImageResponse(
+//     (
+//       <div
+//         style={{
+//           width: '100%',
+//           height: '100%',
+//           display: 'flex',
+//           flexDirection: 'column',
+//           alignItems: 'center',
+//           justifyContent: 'center',
+//           backgroundColor: '#e7d9be',
+//           color: '#453227',
+//           fontSize: 48
+//         }}
+//       >
+//         <img
+//           src={logoSrc}
+//           width='250'
+//         />
+//         <p
+//           style={{
+//             display: 'flex',
+//             alignItems: 'center',
+//             justifyContent: 'center',
+//             gap: 16
+//           }}
+//         >
+//           <svg
+//             style={{marginTop: '6px'}}
+//             xmlns='http://www.w3.org/2000/svg'
+//             width='42'
+//             height='42'
+//             viewBox='0 0 24 24'
+//             fill='none'
+//             stroke='currentColor'
+//             strokeWidth='2'
+//             strokeLinecap='round'
+//             strokeLinejoin='round'
+//           >
+//             <path d='M15 21v-8a1 1 0 0 0-1-1h-4a1 1 0 0 0-1 1v8' />
+//             <path d='M3 10a2 2 0 0 1 .709-1.528l7-5.999a2 2 0 0 1 2.582 0l7 5.999A2 2 0 0 1 21 10v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z' />
+//           </svg>
+//           <span>{`${t('home')} • Mocca Living`}</span>
+//         </p>
+//       </div>
+//     ),
+//     {
+//       fonts: [
+//         {
+//           name: 'Commissioner',
+//           data: commisionerSemiBold,
+//           style: 'normal'
+//         }
+//       ]
+//     }
+//   )
+// }
