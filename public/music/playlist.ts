13a51381198591b61719bdfd7e6332b3590fda4f
chrismitsdev <chrismits88@gmail.com>
2025-06-02 20:56:41 +0300

    chore: updated deps refactor: changed songs for the in-site player

--- public/music/playlist.ts
+++ public/music/playlist.ts
@@ -1,79 +1,13 @@
 const songs = [
   {
-    artist: 'FASSounds',
-    title: 'Calm',
-    src: '/music/calm.mp3'
+    artist: 'Chill Story Cafe',
+    title: 'Morning coffee',
+    src: '/music/morning-coffee-playlist.mp3'
   },
   {
-    artist: 'FASSounds',
-    title: 'Daily coffee',
-    src: '/music/daily-coffee.mp3'
-  },
-  {
-    artist: 'FASSounds',
-    title: 'Good night',
-    src: '/music/good-night.mp3'
-  },
-  {
-    artist: 'FASSounds',
-    title: 'Satisfying',
-    src: '/music/satisfying.mp3'
-  },
-  {
-    artist: 'FASSounds',
-    title: 'Tasty',
-    src: '/music/tasty.mp3'
-  },
-  {
-    artist: 'Lofium',
-    title: 'Backyard',
-    src: '/music/backyard.mp3'
-  },
-  {
-    artist: 'Lofium',
-    title: 'Memories',
-    src: '/music/memories.mp3'
-  },
-  {
-    artist: 'Lofium',
-    title: 'Jinsei',
-    src: '/music/jinsei.mp3'
-  },
-  {
-    artist: 'Lofium',
-    title: 'Toybox',
-    src: '/music/toybox.mp3'
-  },
-  {
-    artist: 'Pumpupthemind',
-    title: 'Chill lofi',
-    src: '/music/chill-lofi.mp3'
-  },
-  {
-    artist: 'Saavane',
-    title: '21 months',
-    src: '/music/21-months.mp3'
-  },
-  {
-    artist: 'Watrfallkero',
-    title: 'Lofi beat',
-    src: '/music/lofi-beat.mp3'
-  },
-
-  {
-    artist: 'Vital',
-    title: 'Soft Vibes',
-    src: '/music/soft-vibes.mp3'
-  },
-  {
-    artist: 'Tvari',
-    title: 'Tokyo Café',
-    src: '/music/tokyo-cafe.mp3'
-  },
-  {
-    artist: 'Lunar Years',
-    title: 'Cozy Coffeehouse',
-    src: '/music/cozy-coffeehouse.mp3'
+    artist: 'Deep Music',
+    title: 'Nightborn secrets',
+    src: '/music/nightborn-secrets-playlist.mp3'
   }
 ].map((entry, i) => ({trackId: i + 1, ...entry})) as Song[]
 
