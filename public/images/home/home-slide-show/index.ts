facefc85e48bd1cf18f9e9aa39ee7343021ad0d6
chrismitsdev <chrismits88@gmail.com>
2025-06-11 10:04:10 +0300

    feat: replaced home-carousel & home-gallery photos chore: updated deps

--- public/images/home/home-slide-show/index.ts
+++ public/images/home/home-slide-show/index.ts
@@ -1,7 +1,7 @@
 export {default as slideShowImage1} from './1.webp'
 export {default as slideShowImage2} from './2.webp'
 export {default as slideShowImage3} from './3.webp'
-export {default as slideShowImage4} from './4.webp'
+
 export {default as slideShowImage5} from './5.webp'
 export {default as slideShowImage6} from './6.webp'
 export {default as slideShowImage7} from './7.webp'
@@ -10,5 +10,5 @@ export {default as slideShowImage9} from './9.webp'
 export {default as slideShowImage10} from './10.webp'
 export {default as slideShowImage11} from './11.webp'
 export {default as slideShowImage12} from './12.webp'
-export {default as slideShowImage13} from './13.webp'
+
 export {default as slideShowImage14} from './14.webp'
