f8bb60cfcd157279eeb525c8114f8a643e043487
chrismitsdev <chrismits88@gmail.com>
2025-06-30 14:08:23 +0300

    fix: generateMetadata function in home page

--- src/app/[locale]/(home)/page.tsx
+++ src/app/[locale]/(home)/page.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react'
+import type {Metadata} from 'next'
 import {getTranslations, setRequestLocale} from 'next-intl/server'
 import {HomeHeader} from '@/src/app/[locale]/(home)/(components)/home-header'
 import {HomeCarousel} from '@/src/app/[locale]/(home)/(components)/home-carousel'
@@ -7,7 +8,7 @@ import {HomeReasons} from '@/src/app/[locale]/(home)/(components)/home-reasons'
 import {HomeDistances} from '@/src/app/[locale]/(home)/(components)/home-distances'
 import {HomeGallery} from '@/src/app/[locale]/(home)/(components)/home-gallery'
 
-export async function generateMetadata({params}: Params) {
+export async function generateMetadata({params}: Params): Promise<Metadata> {
   const {locale} = await params
   const t = await getTranslations({locale, namespace: 'Metadata.Pages'})
 
